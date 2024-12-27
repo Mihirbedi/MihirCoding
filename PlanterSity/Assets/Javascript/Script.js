a10fcb9604e5922b43eb737e0cf24b8649dabe8d
Mihir Bedi <Mihirbedi724@gmail.com>
2024-12-27 13:12:14 +0530

    Add files via upload

---
+++ PlanterSity/Assets/Javascript/Script.js
@@ -0,0 +1,74 @@
+document.addEventListener("mousemove", parallax);
+
+function parallax(event) {
+  document.querySelectorAll("header img").forEach((shift) => {
+    const position = shift.getAttribute("value");
+    const x = (window.innerWidth - event.pageX * position) / 90;
+    const y = (window.innerHeight - event.pageY * position) / 90;
+
+    shift.style.transform = `translateX(${x}px) translateY(${y}px)`;
+  });
+};
+
+let nextButton = document.getElementById('next');
+let prevButton = document.getElementById('prev');
+let carousel = document.querySelector('.carousel');
+let listHTML = document.querySelector('.carousel .list');
+let seeMoreButtons = document.querySelectorAll('.seeMore');
+let backButton = document.getElementById('back');
+
+nextButton.onclick = function(){
+    showSlider('next');
+}
+prevButton.onclick = function(){
+    showSlider('prev');
+}
+let unAcceppClick;
+const showSlider = (type) => {
+    nextButton.style.pointerEvents = 'none';
+    prevButton.style.pointerEvents = 'none';
+
+    carousel.classList.remove('next', 'prev');
+    let items = document.querySelectorAll('.carousel .list .item');
+    if(type === 'next'){
+        listHTML.appendChild(items[0]);
+        carousel.classList.add('next');
+    }else{
+        listHTML.prepend(items[items.length - 1]);
+        carousel.classList.add('prev');
+    }
+    clearTimeout(unAcceppClick);
+    unAcceppClick = setTimeout(()=>{
+        nextButton.style.pointerEvents = 'auto';
+        prevButton.style.pointerEvents = 'auto';
+    }, 2000)
+}
+seeMoreButtons.forEach((button) => {
+    button.onclick = function(){
+        carousel.classList.remove('next', 'prev');
+        carousel.classList.add('showDetail');
+    }
+});
+backButton.onclick = function(){
+    carousel.classList.remove('showDetail');
+}
+
+
+let closeForm = document.querySelector('.popupform i');
+let popupForm = document.querySelector('.popupform');
+let subbtn = document.getElementById('subbtn');
+
+closeForm.onclick = function(){
+    popupForm.style.display = 'none';
+}
+subbtn.onclick = function(){
+    popupForm.style.display = 'none';
+}
+
+
+const accordionItems = document.querySelectorAll('.accordion-item');
+accordionItems.forEach(item => {
+    item.addEventListener('click', () => {
+        item.classList.toggle('active');
+    });
+});
